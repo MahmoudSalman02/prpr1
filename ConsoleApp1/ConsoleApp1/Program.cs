deb206992207ea4aebf119d657d7156954ccf0cb
MahmoudSalman02 <54432196+MahmoudSalman02@users.noreply.github.com>
2019-09-10 13:46:09 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,9 +4,36 @@ namespace ConsoleApp1
 {
     class Program
     {
+    using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine("Välkommen till ett spel där du gissar nummer?");
+            Random randomerare = new Random();
+            int slump_tal = randomerare.Next(1, 100);
+            Console.WriteLine("Gissa på ett tal mellan 1 och 101");
+            string str = Console.ReadLine();
+            int tal = Convert.ToInt32(str);
+
+            do
+            {
+                Console.WriteLine("Fel gissa igen!");
+                str = Console.ReadLine();
+                tal = Convert.ToInt32(str);
+                if (tal > slump_tal)
+                {
+                    Console.WriteLine("Talet är mindre");
+                }
+                else if (tal < slump_tal)
+                {
+                    Console.WriteLine("Talet är större");
+                }
+
+            }
+            while (tal != slump_tal);
+            Console.WriteLine("Grattis du gissade rätt");
+            Console.ReadLine();
